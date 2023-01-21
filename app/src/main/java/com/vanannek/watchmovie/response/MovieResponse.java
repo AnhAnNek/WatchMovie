0b47b80a7643307f18e98ff83828db796f5a4550
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2023-01-21 22:48:49 +0700

    #2 Retrofit response object

---
+++ app/src/main/java/com/vanannek/watchmovie/response/MovieResponse.java
@@ -0,0 +1,24 @@
+package com.vanannek.watchmovie.response;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.vanannek.watchmovie.models.Movie;
+
+// This class is for single movie request
+public class MovieResponse {
+    // 1 - Finding the Movie Object
+    @SerializedName("results")
+    @Expose
+    private Movie movie;
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieResponse{" +
+                "movie=" + movie +
+                '}';
+    }
+}
